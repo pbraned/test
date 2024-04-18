cfe14576d42c00b40920ce07328179c0c2d52f8d
pbraned <167301410+pbraned@users.noreply.github.com>
2024-04-17 22:20:28 -0400

    Update hash.py

--- app/hash.py
+++ app/hash.py
@@ -2,30 +2,33 @@ from PIL import Image
 import imagehash
 import sys
 
-hashmethod = sys.argv[1] if len(sys.argv) > 1 else 0
-if hashmethod == 'ahash':
+def process_arguments(arg1, arg2, arg3):
+    # Your script logic here
+
+    hashmethod = arg1
+    if hashmethod == 'ahash':
         hashfunc = imagehash.average_hash
-elif hashmethod == 'phash':
+    elif hashmethod == 'phash':
         hashfunc = imagehash.phash
-elif hashmethod == 'dhash':
+    elif hashmethod == 'dhash':
         hashfunc = imagehash.dhash
-elif hashmethod == 'whash-haar':
+    elif hashmethod == 'whash-haar':
         hashfunc = imagehash.whash
-#elif hashmethod == 'whash-db4':
-#       def hashfunc(img):
-#               return imagehash.whash(img, mode='db4')
-elif hashmethod == 'colorhash':
+    #elif hashmethod == 'whash-db4':
+    #       def hashfunc(img):
+    #               return imagehash.whash(img, mode='db4')
+    elif hashmethod == 'colorhash':
         hashfunc = imagehash.colorhash
-elif hashmethod == 'crop-resistant':
+    elif hashmethod == 'crop-resistant':
         hashfunc = imagehash.crop_resistant_hash
 
-hash = hashfunc(Image.open(sys.argv[2]))
-otherhash = hashfunc(Image.open(sys.argv[3]))
-#print(hash)
-#print(otherhash)
-hamm = hash - otherhash
+    hash = hashfunc(Image.open(arg2))
+    otherhash = hashfunc(Image.open(arg3))
+    #print(hash)
+    #print(otherhash)
+    hamm = hash - otherhash
 
-if hamm <= int(sys.argv[4]):
-  print(sys.argv[2] + " and "+ sys.argv[3] + " = "+ str(hamm))  # hamming distance
+    #if hamm <= int(sys.argv[4]):
+    #  print(sys.argv[2] + " and "+ sys.argv[3] + " = "+ str(hamm))  # hamming distance
 
-return(hamm)
+    return hamm
